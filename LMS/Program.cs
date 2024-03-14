231585b4c4722dfb4d4093d906dc9ef8fc0cac4a
dbarlo2 <danielle.barlow@jefferson.kyschools.us>
2024-03-14 11:20:00 -0400

    transfer

--- LMS/Program.cs
+++ LMS/Program.cs
@@ -1,7 +1,25 @@
+using System;
+using IronBarCode;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 app.MapRazorPages();
 app.Run();
+
+namespace IronBarCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var barcode = BarcodeWriter.CreateBarcode("JCPS barcode", BarcodeEncoding.Code128);
+            barcode.AddBarcodeValueTextBelowBarcode();
+            barcode.SaveAsPng(("Barcode.png"));
+        }
+    }
+}
